21379c7a7c208760263d1eec35239e82a78d3f47
David Lam <david@buddybuild.com>
2017-03-04 14:22:31 -0800

    Add gitignore and fbsnapshotetestcase skeleton

---
+++ m2048Tests/m2048FBSnapshotTestCases.m
@@ -0,0 +1,38 @@
+//
+//  m2048FBSnapshotTestCases.m
+//  m2048
+//
+//  Created by David Lam on 2017-03-04.
+//  Copyright © 2017 Danqing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
+
+@interface m2048FBSnapshotTestCases : FBSnapshotTestCase
+
+@end
+
+@implementation m2048FBSnapshotTestCases
+
+- (void)setUp
+{
+    [super setUp];
+    // Flip this to YES to record images in the reference image directory.
+    // You need to do this the first time you create a test and whenever you change the snapshotted views.
+    // Tests running in record mode will allways fail so that you know that you have to do something here before you commit.
+    self.recordMode = NO;
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testViewSnapshot
+{
+    
+}
+
+@end
